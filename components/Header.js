08bebea1e59627bf4523731c52a34cee52bdf6a6
Dnyanesh Bachhav <dnyaneshbachhav2002@gmail.com>
2022-05-22 11:47:14 +0530

    Normal Code refractoring...

--- components/Header.js
+++ components/Header.js
@@ -14,6 +14,7 @@ function Header(){
     <Text style={styles.textStyle}>CoinStack</Text>
     <View style={styles.iconStyle}>
     <TouchableOpacity>
+      {/* Add Element */}
       {/* <AntDesign name="caretdown" size={21} color={COLORS.white}/> */}
     </TouchableOpacity>
     </View>
