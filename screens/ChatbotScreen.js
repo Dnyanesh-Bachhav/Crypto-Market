43d5430e6f188ef2323fbca1f6b6861e5cf6c3f8
Dnyanesh Bachhav <dnyaneshbachhav2002@gmail.com>
2022-12-21 22:00:08 +0530

    Added comments in chatbot screen...

--- screens/ChatbotScreen.js
+++ screens/ChatbotScreen.js
@@ -1,12 +1,19 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import DrawerScreenHeader from "../components/HomeScreen/DrawerScreenHeader";
+
+// Chatbot Screen powered by ChatGPT
 
 function ChatbotScreen(){
     return(
-        <View>
+        <View style={styles.container}>
             <DrawerScreenHeader headerText={"Chatbot"} />
             <Text>Hello World...!!!</Text>
         </View>
     );
 }
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+    }
+})
 export default ChatbotScreen;
