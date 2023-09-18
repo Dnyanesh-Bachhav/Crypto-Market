85275564c88af23cf6a7a1fc2b38b4e4bdfa69c0
Dnyanesh Bachhav <dnyaneshbachhav2002@gmail.com>
2023-09-19 02:36:33 +0530

    Minor updates...

--- App.js
+++ App.js
@@ -30,7 +30,6 @@ export default function App() {
       <>
       <PortfolioContextProvider>
         <TransactionContextProvider>
-
         <NavigationContainer>
             <Tabs/>
         </NavigationContainer>
